428f5fd2e9e445d2d38b66e29645e04469d530a9
Paul Thompson <yakulbeans@gmail.com>
2020-09-05 20:05:22 -0600

    added movement with clamping to edges, and r for restart

--- Machine.lua
+++ Machine.lua
@@ -10,13 +10,17 @@ local xoffset
 machineAtlas = love.graphics.newImage('graphics/Machine1_SpriteSheet.png')
 machineSprite = love.graphics.newQuad(0, 0, 200, 350, machineAtlas:getDimensions())
 
+local machineSpeed = 150
+
 function Machine:init()
 	self.image = love.graphics.newImage('graphics/Machine1_SpriteSheet.png')
 	self.x = 300
 	self.y = 100
+	self.dx = 0
 end
 
 function Machine:update(dt)
+	--animation
 	animationTimer = animationTimer - dt
 	if animationTimer <= 0 then
 		animationTimer = 1 / fps
@@ -24,6 +28,23 @@ function Machine:update(dt)
 		if frame > totalFrames then frame = 1 end
 		xoffset = 200 * (frame - 1)
 		machineSprite:setViewport(xoffset, 0, 200, 350)
+	end
+
+	--input handling
+	if love.keyboard.isDown('left') then
+		self.dx = -machineSpeed * dt
+		self.x = math.max(self.x + self.dx, -20)
+	end
+
+	if love.keyboard.isDown('right') then
+		self.dx = machineSpeed * dt
+		self.x = math.min(self.x + self.dx, VIRTUAL_WIDTH - 180)
+	end
+
+	if love.keyboard.wasPressed('r') then
+		self.x = 300
+		self.y = 100
+		self.dx = 0
 	end
 end
 
